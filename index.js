ea1109d8a35c36069ab0a873c0aac55686fbaec3
karlossg <karol.gratus@gmail.com>
2018-12-19 22:10:10 +0100

    added window resizing and some jsdoc comments

--- index.js
+++ index.js
@@ -1,28 +1,71 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
+
+let game;
+
+/**
+ *
+ * Function used to load game config and resazing function
+ * @name loadGameConfig
+ * @function
+ *
+ */
+
+function loadGameConfig() {
+  const config = {
+    type: Phaser.AUTO,
+    width: 1334,
+    height: 750,
+    scene: Game,
+    backgroundColor: "#5B5678",
+    physics: {
+      default: "arcade"
+    }
+  };
+
+  game = new Phaser.Game(config);
+  window.focus();
+  resizeGame();
+  window.addEventListener("resize", resizeGame);
+}
+
+window.onload = () => loadGameConfig();
+
+/**
+ *
+ *
+ * @class Game
+ * @extends {Phaser.Scene}
+ */
 class Game extends Phaser.Scene {
   constructor() {
-    super('Game')
+    super("Game");
   }
-  preaload() {
-    // this.add.text(100, 100, 'Phaser + Parcel');
-  }
+  preaload() {}
   create() {
-    this.add.text(100, 100, 'Phaser + Parcel');
+    this.add.text(100, 100, "Phaser + Paasdasdrcel");
   }
 }
 
-const config = {
-  type: Phaser.AUTO,
-  width: 1440,
-  height: 800,
-  scene: Game,
-  backgroundColor: '#5B5678',
-  physics: {
-    default: "arcade"
-  },
-};
-
-new Phaser.Game(config);
+/**
+ * Game window resazing function
+ * @name resizeGame
+ * @function
+ *
+ */
+function resizeGame() {
+  let canvas = document.querySelector("canvas");
+  let windowWidth = window.innerWidth;
+  let windowHeight = window.innerHeight;
+  let windowRatio = windowWidth / windowHeight;
+  let gameRatio = game.config.width / game.config.height;
+  if (windowRatio < gameRatio) {
+    canvas.style.width = windowWidth + "px";
+    canvas.style.height = windowWidth / gameRatio + "px";
+  } else {
+    canvas.style.width = windowHeight * gameRatio + "px";
+    canvas.style.height = windowHeight + "px";
+  }
+}
 
 if (module.hot) {
   module.hot.accept(() => {});
@@ -31,4 +74,3 @@ if (module.hot) {
     window.location.reload();
   });
 }
-
